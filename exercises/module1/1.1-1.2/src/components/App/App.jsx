0955f9885807a785f089aa2445dcb09fa4c41e49
Laurent Vandermeersch <laurent.vandermeersch@student.vinci.be>
2023-09-22 10:03:10 +0200

    1.1-1.2 : CSS style

--- exercises/module1/1.1-1.2/src/components/App/App.jsx
+++ exercises/module1/1.1-1.2/src/components/App/App.jsx
@@ -2,6 +2,7 @@
 import Content from "components/Content/Content"
 import Header from "components/Header/Header"
 import Total from "components/Total/Total"
+import './App.css'
 
 const App = () => {
   // const-definitions
