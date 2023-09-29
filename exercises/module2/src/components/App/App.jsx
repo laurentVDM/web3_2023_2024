16a2fb69b517bbd15a9ebc3548880badc0be2387
Laurent Vandermeersch <laurent.vandermeersch@student.vinci.be>
2023-09-29 09:15:01 +0200

    module2 : localStorage

--- exercises/module2/src/components/App/App.jsx
+++ exercises/module2/src/components/App/App.jsx
@@ -4,10 +4,11 @@ import { useState } from 'react';
 
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(JSON.parse(localStorage.getItem("counter")));
 
   const changeCount = (delta) => {
     setCounter(counter + delta);
+    localStorage.setItem("counter", JSON.stringify(counter + delta));
   };
 
   return (
