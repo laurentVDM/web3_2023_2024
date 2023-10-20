58092f58e52391ea02b3b93bd2e044a6925fb57a
Laurent Vandermeersch <laurent.vandermeersch@student.vinci.be>
2023-10-20 11:19:05 +0200

    module5 : ex 5.2

---
+++ exercises/module5/5.2/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import AddOpinions from 'components/AddOpinion/AddOpinion';
+import ListOpinions from 'components/ListOpinion/ListOpinion';
+
+const App = () => {
+  return (
+    <div>
+      <ListOpinions />
+      <AddOpinions />
+    </div>
+  );
+};
+
+export default App;
